f66ba783941fdd8ae8ab5ac980de208935bd1a63
MLGC00KIE <24893@ma-web.nl>
2018-03-16 14:00:05 +0100

    updated hallo.js

--- hallo.js
+++ hallo.js
@@ -1,1 +1,21 @@
+// print line in console
 console.log("hallo wereld");
+
+
+// create random number
+var RNG = Math.floor((Math.random() * 10) + 1);
+
+// check if the number is under or equals 4
+if (RNG <= 4) {
+  console.log("het nummer is 4 of lager");
+  var win = false;
+} else {
+  console.log("het nummer is: " + RNG);
+  var win = true;
+}
+
+if (win) {
+  console.log("congratulations you won nothing");
+} else {
+  console.log("you lost noob");
+}
